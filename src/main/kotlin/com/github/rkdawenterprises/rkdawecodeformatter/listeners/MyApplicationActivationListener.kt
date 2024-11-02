d304f6e74ab268348daa5b195155145bf22e608b
GitHub Action <action@github.com>
2024-11-02 21:21:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/rkdawenterprises/rkdawecodeformatter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/rkdawenterprises/rkdawecodeformatter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rkdawenterprises.rkdawecodeformatter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
